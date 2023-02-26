b02e52ec9e6076ffc7abaf329897297e3f29debd
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2023-02-26 22:12:15 +0100

    init commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>test</Text>
       <StatusBar style="auto" />
     </View>
   );
