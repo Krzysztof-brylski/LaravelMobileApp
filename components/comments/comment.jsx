1384409e4b364d7850067c81c7827d448807551c
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2023-03-05 19:36:43 +0100

    implemented liking comments

--- components/comments/comment.jsx
+++ components/comments/comment.jsx
@@ -1,13 +1,35 @@
 import * as React from "react";
-import { StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View,Button} from "react-native";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
 import CommentAuthor from "./commentAuthor";
+import AxiosFacade from "../../facades/Axios";
 
 
 const CommentComponent=({item, navigation })=>{
+
+    const [liked,setLiked]=React.useState(item.liked);
+    const [likes,setLikes]=React.useState(item.likesCount);
     const viewProfile=()=>{
         navigation.navigate('Profile',{'id':item.author.id});
+    };
+
+    const like=()=>{
+
+        AxiosFacade.build().post(`/comment/like/${item.id}`).then((res)=>{
+
+            if(!liked){
+                setLikes(likes+1);
+                setLiked(true);
+            }else{
+
+                setLikes(likes-1);
+                setLiked(false);
+            }
+
+        }).catch((err)=>{
+            console.error(err);
+        });
     };
 
     return (
@@ -20,12 +42,18 @@ const CommentComponent=({item, navigation })=>{
                         username={item.author.username}
                     />
                 </TouchableOpacity>
-                <FontAwesomeIcon icon={faHeart} size={25} style={{paddingHorizontal: 25}}/>
+                <TouchableOpacity onPress={like}>
+                    <FontAwesomeIcon icon={faHeart} size={25} style={[style.btn, liked ?style.likedBtn:null ]}/>
+                </TouchableOpacity>
             </View>
             <View style={style.contentContainer}>
                 <Text>
                     {item.content}
                 </Text>
+            </View>
+            <View style={{flexDirection:"row",marginVertical:10}}>
+                <Text  style={style.subtitles}> Likes: {likes}</Text>
+                <Text  style={style.subtitles}> Answer</Text>
             </View>
         </View>
     );
@@ -43,6 +71,18 @@ const style =  StyleSheet.create({
     },
     contentContainer:{
         marginHorizontal:25,
+    },
+    subtitles:{
+        color:"gray",
+        fontSize:15,
+        fontWeight:"bold",
+        paddingHorizontal:15
+    },
+    btn:{
+        paddingHorizontal: 25,
+    },
+    likedBtn:{
+        color:"red",
     }
 });
 
